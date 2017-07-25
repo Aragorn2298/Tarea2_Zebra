d1c981d10b967f293ce5aba0822e5f9233c95804
Aragorn2209 <ramonbust2209@gmail.com>
2017-07-25 16:39:19 -0600

    Tarea2_Zebra
    Tarea de la semana 2

---
+++ cmake-build-debug/CMakeFiles/Tarea2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea2.dir/main.cpp.obj"
+  "CMakeFiles/Tarea2.dir/Test.cpp.obj"
+  "Tarea2.pdb"
+  "Tarea2.exe"
+  "Tarea2.exe.manifest"
+  "libTarea2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+//Desrefencia ptr (dado) y devuelve su valor
+int obtenerValor(int *ptr)
+{
+    return *ptr;
+}
+
+//Desreferencia a (dado) y b (dado) y devuelve la suma de ambos
+int sumar(int* a, int* b)
+{
+    return *a+*b;
+}
+
+//Devuelve true si a (dado) y b (dado) apuntan al mismo espacio de memoria
+bool compararApuntadores(string *a, string *b)
+{
+    if(* &a==* &b){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//Desrefencia a (dado) y b (dado), devuelve true si ambos valores son iguales de lo contrario devuelve false
+bool comparar(string *a, string *b) {
+    if (*a == *b){
+    return true;
+    }else{
+        return false;
+    }
+
+}
+
+//Desreferencia a (dado) y devuelve su primera letra
+char getPrimeraLetra(string* a)
+{
+    char primera = a->at(0);
+    return primera;
+}
+
+//Desreferencia a (dado) y devuelve true si es palindroma de lo contrario devuelve false
+//Definicion de palindromo: http://es.wikipedia.org/wiki/Pal%C3%ADndromo
+//Ejemplos de palindromos: Ana, arenera, arepera, anilina, ananá, Malayalam, Neuquén, Oruro, oso, radar, reconocer, rotor, salas, seres, somos, sometemos
+bool esPalindromo(string* a)
+{
+    string *alreve = new string("");
+    for (int c=a->length()-1;c>=0;c--){
+        alreve +=a->at(c);
+    }if(alreve==a){
+        return true;
+    }
+}
+
+//Desreferencia base (dado) y exponente (dado) y devuelve la base elevado al exponente
+int getExponente(int* base, int* exponente)
+{
+    int x=1;
+    for(int c=0;c<*exponente;c++){
+        x=x*(*base);
+    }
+    return x;
+}
+
+int main () {
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
